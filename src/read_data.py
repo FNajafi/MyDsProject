9f2416def60ccf1ea579c0bb4ed6b952ffe9e022
Fridoon <fridoondehqan123@gmail.com>
2023-10-19 08:05:40 +0200

    deleted the files

--- src/read_data.py
+++
@@ -1,6 +0,0 @@
-import pandas as pd
-df = pd.read_csv("../data/osi.csv")
-df.head (5)
-df.tail(10)
-print ("this was a simpel code to creat a data frame")
-print ("This is to check the commit")
--- src/test.py
+++
@@ -1,6 +0,0 @@
-a = 80
-b = 20 
-c = a + b
-print ("This is the value of c" , c)
-d = a * b 
-print ("the multiplication is equal to ", d )
